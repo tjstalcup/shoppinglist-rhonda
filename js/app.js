275a7b615c9f4b14d87d636b37b8e65e6f0ec096
rhondarobot <rrachellej@gmail.com>
2016-05-17 16:33:43 -0700

    cleaning up code--redoing it

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-	$('#user-input').submit(function(event) {
+	$("item").submit(function(event) {
 		event.preventDefault();
-		var something = $('inputBox').val();
+		var something = $("input").val();
 		//if user inputs no text and hits 'enter', alert will pop up and nothing is appended:
 		if(something === "") {
               alert('Please enter an item!')
 		}
 		//otherwise, text is appended as a li to the shopping list:
 		else {
-			var "" = $('inputBox').val();
-			$('.shopping-list-items ul').append("<li class='current-item'><img class='btn-purchased' src=''/>" + "" + eggs + "<img src='' class='remove'/></li>");
+			var "" = $("input").val();
+			$('.list ul').append("<li class="items"><div class="purchased" src=''/>" + "" + something + "<div class="remove"></li>");
 		     }
 		     //resets the textbox after item is entered:
-		     $('#user-input')[0].reset();
+		     $('#item')[0].reset();
 		
 	});
 });
@@ -20,12 +20,11 @@ $(document).ready(function() {
 //functions to delete and strikethrough code when purchased and remove buttons are clicked 
 assignlisteners();
 function assignlisteners() {
-	$('ul').on('click','.btn-remove',function(event) {
-		console.log('.btn-remove clicked');
+	$('ul').on('click','.remove',function(event) {
+		console.log('.remove clicked');
 		$(this).closest('li').remove();
 	})
 }
-    $('ul').on('click','.btn-purchased',function(event) {
+    $('ul').on('click','.purchased',function(event) {
     	$(this).closest('li').toggleClass('linethrough');
-    	console.log('btn-purchased clicked');
     })
