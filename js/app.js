ea792d99e7156760f073365d538ada55948a084d
rhondarobot <rrachellej@gmail.com>
2016-05-25 12:27:04 -0700

    Items are now linked up to remove all purchased button. Now to style

--- js/app.js
+++ js/app.js
@@ -2,44 +2,41 @@ $(document).ready(function() {
   $('#groceryItem').on('submit', function(event) {
     event.preventDefault();  
    
-   var newItem = $('#inputText').val();  
+     var newItem = $('#inputText').val();  
      if (newItem === "") {
-       alert("Please enter an item first!")
-     }
-    else {
-       $('ul').append('<li>' + '<input type="checkbox">' + newItem + '</li>');   
-     $('#inputText').val('');
-    }
-    }); 
+        alert("Please enter an item first!")
+  }  else {
+        $('ul').append('<li>' + '<input type="checkbox">' + newItem + '</li>');   
+        $('#inputText').val('');
+  }
+}); 
   //will not add a blank item when alert is shown and will only add <li>s upon entering text
 
-  $(document).on('click', 'li input:checkbox', function() {
+$(document).on('click', 'li input:checkbox', function() {
      console.log(this);
-    var $this = $(this);
-    if (this.checked) {
-      $this.parent().addClass('purchased');
-      } else {
-        $this.parent().removeClass('purchased')
-};
+      var $this = $(this);
+      if (this.checked) {
+         $this.parent().addClass('purchased');
+  }   else {
+         $this.parent().removeClass('purchased')
+ };
+ });
+   
+ $('#removeAll').on('click',function() {
+   console.log('remove all clicked');
+   $('.purchased').remove();
+ }); 
+
+$('#sortable').on('mouseenter').sortable({ axis: "y" });
+  
+  
+
+
+}); //end of jQuery
+  
+
     
-    var purchasedItem = "$this.parent().addClass('purchased');";
- $('document').on('click','input:button',function() {
-   $('input:button').remove(purchasedItem);
- });   
 
-
-  
- 
-    
-});
-
- 
-
-  
-  });
-  
-//add remove all checked/crossed out items button
-//sort items if needed
     
 
     
